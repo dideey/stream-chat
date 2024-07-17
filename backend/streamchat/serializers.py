d642fa80e33400e6412b01fb2f6757011cbdd094
Karl <tekamkarl@gmail.com>
2024-07-17 10:17:39 +0100

    serialisztion

---
+++ backend/streamchat/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'password']
+
+    def create(self, validated_data):
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            password=validated_data['password']
+        )
+        return user
+
+class LoginSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    password = serializers.CharField()
