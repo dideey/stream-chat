c2ca2320347071f46d15321fd75a926313c9fa1d
Karl <tekamkarl@gmail.com>
2024-07-17 10:18:45 +0100

    add urls

--- backend/streamchat/urls.py
+++ backend/streamchat/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from django.contrib.auth import views as auth_views
 from . import views
+from .views import SignupView, LoginView, LogoutView
 
 urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('login/', views.customlogin, name='login'),
     path('logout/', views.customlogout, name='logout'),
 
+    path('api/signup/', SignupView.as_view(), name='signup'),
+    path('api/login/', LoginView.as_view(), name='login'),
+    path('api/logout/', LogoutView.as_view(), name='logout'),
 ]
