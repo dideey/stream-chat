7f7e74fa2ab5e30cdf9fedb639bf48e6c4be833a
Karl <tekamkarl@gmail.com>
2024-07-17 08:45:01 +0100

    add url for REST framework's login and logout views

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -18,6 +18,7 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
+    path('api-auth/', include('rest_framework.urls')),
     path('admin/', admin.site.urls),
     path('', include('streamchat.urls')),
     path('accounts/', include('allauth.urls'))
