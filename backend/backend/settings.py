11955ae4c8712e6470a9b993e0155fe48d4f2c41
Karl <tekamkarl@gmail.com>
2024-07-19 05:44:56 +0100

    changing database sqlite to postgresql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -92,8 +92,12 @@ CHANNEL_LAYERS = {
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        "NAME": "streamchat",
+        "USER": "karl",
+        "PASSWORD": "adminkarl",
+        "HOST": "127.0.0.1",
+        "PORT": "5432",
     }
 }
 
