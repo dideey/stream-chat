a9a5f57d8ed8258efa19fa5050e034c90f1f3bfe
Karl <tekamkarl@gmail.com>
2024-07-20 11:35:31 +0100

    register ProfileSerializer

--- backend/streamchat/urls.py
+++ backend/streamchat/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, GroupViewSet
+from .views import UserViewSet, GroupViewSet, ProfileViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
+router.register(r'profile', ProfileViewSet)
 
 # urls.py
 urlpatterns = [
