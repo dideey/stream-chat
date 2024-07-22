c435da88496f4ea33a83a1ab87c59a8d43c47c9b
Karl <tekamkarl@gmail.com>
2024-07-22 18:11:49 +0100

    added toute for chat

--- backend/streamchat/urls.py
+++ backend/streamchat/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import UserViewSet, GroupViewSet, ProfileViewSet
+from .views import UserViewSet, GroupViewSet, ProfileViewSet, ChatViewSet
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
 router.register(r'groups', GroupViewSet)
 router.register(r'profile', ProfileViewSet)
+router.register(r'chat', ChatViewSet)
 
 # urls.py
 urlpatterns = [
