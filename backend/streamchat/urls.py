a02ee1ba5c3004e761c881ea582137884f87f5fa
Karl <tekamkarl@gmail.com>
2024-07-26 11:49:38 +0100

    added token

--- backend/streamchat/urls.py
+++ backend/streamchat/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from .views import UserViewSet, GroupViewSet, ProfileViewSet, ChatViewSet
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+    TokenVerifyView,
+)
+
 
 router = DefaultRouter()
 router.register(r'users', UserViewSet)
@@ -11,5 +17,8 @@ router.register(r'chat', ChatViewSet)
 # urls.py
 urlpatterns = [
     path('', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls'))
+    path('api-auth/', include('rest_framework.urls')),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
 ]
